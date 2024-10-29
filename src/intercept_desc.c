01ec7a7eeab04afba0d33b542b6b10e150cb333f
Petar Andric <slate@null.net>
2024-10-29 08:49:10 +0000

    Add contributor

--- src/intercept_desc.c
+++ src/intercept_desc.c
@@ -1,5 +1,6 @@
 /*
- * Copyright 2016-2017, Intel Corporation
+ * Copyright 2016-2024, Intel Corporation
+ * Contributor: Petar Andrić
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
